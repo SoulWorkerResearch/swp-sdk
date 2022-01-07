0ae79c22f42c9190358fa13bc5b1d27fa13599ce
sawich <sawich@angeldev.org>
2022-01-07 04:14:48 +0200

    remove dllexport from loader

--- headers/sdk/plugin/plugin_emplace.hpp
+++ headers/sdk/plugin/plugin_emplace.hpp
@@ -5,5 +5,17 @@
 
 namespace sdk::plugin
 {
-  extern "C" __declspec(dllexport) auto emplace(void)->info*;
+#ifdef SWP_SDK_API
+  static_assert("wtf?");
+#endif
+
+#ifndef SWP_LOADER
+#define SWP_SDK_API extern "C" __declspec(dllexport)
+#else
+#define SWP_SDK_API
+#endif
+
+  SWP_SDK_API auto emplace(void)->info*;
+
+#undef SWP_SDK_API
 }
